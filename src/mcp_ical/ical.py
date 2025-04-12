6f44b9445cdf2ea429ff9b84475ea15a1bf9523e
Eric Buess <ericbuess@example.com>
2025-04-11 23:22:50 -0500

    Fix: Revert to standard calendar access method for all macOS versions

--- src/mcp_ical/ical.py
+++ src/mcp_ical/ical.py
@@ -290,18 +290,9 @@ class CalendarManager:
             access_granted = granted
             semaphore.release()
 
-        # Get macOS version
-        mac_version = tuple(map(int, platform.mac_ver()[0].split('.')[:2]))
-        logger.debug(f"macOS version detected: {mac_version}")
-        
-        # Use requestFullAccessToEvents for macOS 14.0+ (Sequoia/15.0+)
-        # Note: The method name is fixed here - removed the trailing underscore
-        if mac_version >= (14, 0):
-            logger.info("Using requestFullAccessToEvents for macOS 14.0+")
-            self.event_store.requestFullAccessToEvents(completion)
-        else:
-            logger.info("Using requestAccessToEntityType_completion_ for older macOS versions")
-            self.event_store.requestAccessToEntityType_completion_(EKEntityTypeEvent, completion)
+        # Use the standard method that works on all macOS versions
+        logger.info("Requesting calendar access using requestAccessToEntityType_completion_")
+        self.event_store.requestAccessToEntityType_completion_(EKEntityTypeEvent, completion)
             
         semaphore.acquire()
         return access_granted
